ae36e339f29f5839eaddf36ec29b42e302402f0a
Leonid Makarov <leonid.makarov@ffwagency.com>
2015-10-25 19:06:29 -0400

    Start NFS client utilities at boot

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -30,6 +30,9 @@ done
 # This only works for interactive sessions, so using symlinking above instead...
 # Add /var/lib/boot2docker/bin to PATH
 #echo 'export PATH=/var/lib/boot2docker/bin:$PATH' | sudo tee -a /etc/profile
+
+# Start nfs client utilities
+sudo /usr/local/etc/init.d/nfs-client start
 SCRIPT
 sudo chmod +x /var/lib/boot2docker/bootsync.sh
 
